b5b548ceb96064077ef193480503215b9fc672b1
Anjosisa <isahanjos1801@gmail.com>
2024-05-21 09:25:48 -0300

    Tentativa e falha da renderização

--- Aula_Teste_DJ/hotel/views.py
+++ Aula_Teste_DJ/hotel/views.py
@@ -96,7 +96,7 @@ def login (request):
 
             if user is not None:
                 return HttpResponse("<h1 style=\"font-family: 'Courier New', Courier, monospace; background-color: #f5c2dac6; text-align: center; padding: 20px; padding-top: 50px; padding-bottom: 50px\">Reserva realizada com sucesso!<br> Em breve entraremos em contato com você para mais detalhes.<br>Obrigada por escolher o Hotel Senai!</h1>")
-        
+
             else:
                 return HttpResponse("<h1 style=\"font-family: 'Courier New', Courier, monospace; background-color: #f5c2dac6; text-align: center; padding: 20px; padding-top: 50px; padding-bottom: 50px\">Usuário ou Senha incorreta</h1>")
 
